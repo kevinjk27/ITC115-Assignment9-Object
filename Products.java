adcbed13da2a38eb546ce0461e0a8afc3db58df8
Kevin Kantono <52363107+kevinjk27@users.noreply.github.com>
2020-03-08 23:45:48 -0700

    Add files via upload

--- Products.java
+++ Products.java
@@ -3,10 +3,10 @@ package assignment9;
 import java.text.NumberFormat;
 
 public class Products {
-	public int productCode;
-	public String description;
-	public double price;
-	public int productCount;
+	private int productCode;
+	private String description;
+	private double price;
+	private int productCount;
 
 	public Products(int productCode, String description, double price, int productCount) {
 		this.productCode = productCode;
@@ -14,34 +14,54 @@ public class Products {
 		this.price = price;
 		this.productCount = productCount;
 	}
-	
+
 	public String getProductDescription() {
 		return this.description;
 	}
-	
+
 	public double getProductPrice() {
 		return this.price;
 	}
-	
-	public int getProductQty(){
+
+	public int getProductQty() {
 		return this.productCount;
 	}
-	
+
 	public void setProductDescription(String description) {
 		this.description = description;
 	}
-	
-	public void setProductPrice (double price) {
+
+	public void setProductPrice(double price) {
 		this.price = price;
 	}
-	
-	public void setProductQty (int productCount) {
+
+	public void setProductQty(int productCount) {
 		this.productCount = productCount;
 	}
-	
+
 	public String getPriceFormatted() {
 		String formattedPrice = NumberFormat.getCurrencyInstance().format(this.price);
 		return formattedPrice;
 	}
-	
+
+	public String toString() {
+		return ("Product # " + productCode + " is " + description + '\n' + "Currently " + productCount + " on hand \n"
+				+ "Price: $" + price);
+
+	}
+
+	public void selling(int sold) {
+		this.productCount = productCount - sold;
+	}
+
+	public void buying(int bought) {
+		this.productCount = productCount + bought;
+	}
+
+	public void discount(double discountedAmount) {
+		double newPrice = (price - discountedAmount / 100 * price);
+		this.price = (Math.round(newPrice * 100.0) / 100.0); // TWO DECIMAL formatting
+	}
+
+
 }
--- useProduct.java
+++ useProduct.java
@@ -4,10 +4,33 @@ public class useProduct {
 
 	public static void main(String[] args) {
 		Products paintbrush = new Products(001, "Painting Paint Brush", 2.99, 5);
-		Products easel = new Products(002, "Cra-Z-Art 3 in 1 Wood Art Easel", 12.39, 7);
+		Products easel = new Products(002, "CraZ Art 3 in 1 Wood Art Easel", 12.39, 7);
 		Products paint = new Products(003, "12 PC Acrylic Paint Set Water Based", 23.99, 2);
+		Products coloringpencil  = new Products(004, "Crayola Eraseable Colored Pencils", 4.99, 3);
 
 		System.out.println(paint.getProductDescription());
+		System.out.println(paint.getPriceFormatted());
+		System.out.println();
+		
+		System.out.println(easel.toString());
+		System.out.println();
+		
+		System.out.println("Updated details after it was sold for 3 pieces");
+		easel.selling(3);
+		System.out.println(easel.toString());
+		System.out.println();
+		
+		System.out.println("Updated details after supplier sends 10 pieces");
+		easel.buying(10);
+		System.out.println(easel.toString());
+		System.out.println();
+		
+		
+		System.out.println("Then the item is discounted for 26% off");
+		easel.discount(26);
+		System.out.println(easel.toString());
+		System.out.println();
+		
 	}
 
 }
